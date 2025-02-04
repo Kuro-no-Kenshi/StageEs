5fe770b1fc3c77d437777b983fd2fc32e6442588
Kuro-no.Kenshi <dani04ranzo@gmail.com>
2025-02-04 11:29:35 +0100

    Recreated the project

---
+++ StageEs/Data/AziendaDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StageEs.Data;
+
+public class AziendaDbContext : DbContext
+{
+    public AziendaDbContext(DbContextOptions<AziendaDbContext> options) : base(options) { }
+
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<TestataDocumento> TestataDocumenti { get; set; }
+    public DbSet<RigaDocumento> RigaDocumenti { get; set; }
+}
