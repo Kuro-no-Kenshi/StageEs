bce6203a822ff76189ce1ec2a489a84166310c3f
Kuro-no.Kenshi <dani04ranzo@gmail.com>
2025-02-04 11:42:45 +0100

    aggiunta connection string e aggiunte cors

--- StageEs/Program.cs
+++ StageEs/Program.cs
@@ -1,15 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddSqlServer<AziendaDbContext>(builder.Configuration.GetConnectionString("Default"));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +26,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
+app.UseCors("AllowAllOrigins");
 
 app.Run();
